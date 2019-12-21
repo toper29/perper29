57031759b8a9f2443814caa25f98f06227905eb6
toper29 <55727583+toper29@users.noreply.github.com>
2019-12-21 11:38:38 +0700

    Link Geo.sh

--- locator.sh
+++ locator.sh
@@ -375,11 +375,11 @@ done
 banner() {
 
 printf "\e[1;93m  _                                                \e[0m\n"
-printf "\e[1;93m (_)                             \e[0m\e[1;77m_                 \e[0m\n"
-printf "\e[1;77m  _        ___    ____  _____  _| |_  ___    ____  \e[0m\n"
-printf "\e[1;77m | |      / _ \  / ___)(____ |(_   _)/ _ \  / ___) \e[0m\n"
-printf "\e[1;77m | |_____| |_| |( (___ / ___ |  | |_| |_| || |     \e[0m\n"
-printf "\e[1;77m |_______)\___/  \____)\_____|   \__)\___/ |_|    v1.1 \e[0m\n"
+printf "\e[1;93m (_)                                   \e[0m\e[1;77m_                 \e[0m\n"
+printf "\e[1;77m  _____   ___   ___   ___   ___        \e[0m\n"
+printf "\e[1;77m |_   _| |   | |   | |     |   |       \e[0m\n"
+printf "\e[1;77m   | |   | | | |___| |---  |___|       \e[0m\n"
+printf "\e[1;77m   |_|   |___| |     |___  |\     v1.1 \e[0m\n"
 printf "\n"
 printf "\e[1;93m :::\e[0m\e[1;77m Coded by: @thelinuxchoice\e[0m\n"
 
